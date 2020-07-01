fa28009b4a583c686fa0f1d53d636bf5680aece7
Tomas Delvechio <tdelvechio@unlu.edu.ar>
2020-06-30 22:15:34 -0300

    debug heroku

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -15,6 +15,7 @@ class Connection
     public static function make($config)
     {
         try {
+            var_dump($config);
             return new PDO(
                 $config['connection'].';dbname='.$config['name'],
                 $config['username'],
